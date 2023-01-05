ec26a3c29a67440a80d0283df5a44879d3cc438d
Sean <sfh1980@yahoo.com>
2023-01-05 15:12:25 -0500

    first

---
+++ madlibs.py
@@ -0,0 +1,20 @@
+
+#this game will use f strings to create the madlibs game
+
+
+word1 = input("word1: ")
+word2 = input("word2: ")
+word3 = input("word3: ")
+word4 = input("word4: ")
+word5 = input("word5: ")
+word6 = input("word6: ")
+word7 = input("word7: ")
+word8 = input("word8: ")
+word9 = input("word9: ")
+word10 = input("word10: ")
+word11 = input("word11: ")
+
+madlib = f"There once was a man from {word1}. They loved playing with {word2}. This one time they {word3} on a {word4} and couldnt stop laughing all the way to {word5}. Then they saw a bright {word6} in the {word7} and began to {word8}. So they drank a {word9} to feel better. The man was then {word10} and lived {word11} ever after."
+
+print(madlib)
+    
